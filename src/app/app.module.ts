8901cfec3f7d0887b3adb558f9e7a5b78ce9fef1
LsDri92 <luciano.s.dri@gmail.com>
2022-05-24 16:05:35 -0300

    Portfolio1 /no services/

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
+import { FormacionComponent } from './components/formacion/formacion.component';
+import { ExpLaboralComponent } from './components/exp-laboral/exp-laboral.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { IniciarSesionComponent } from './components/iniciar-sesion/iniciar-sesion.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SobreMiComponent,
+    FormacionComponent,
+    ExpLaboralComponent,
+    SkillsComponent,
+    FooterComponent,
+    IniciarSesionComponent,
+    PortfolioComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule,
